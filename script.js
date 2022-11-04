78b95c4a733f98d146874da578a026eb310f702b
izima01 <kingsleyizima@gmail.com>
2022-11-04 11:31:07 +0100

    style: smoothed out the dino's jump animation

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ let tree = document.querySelector(".bi-tree-fill")
 let count = 0;
 
 function jump() {
-    document.querySelector("#dino").style = "animation: jump 450ms";
+    dino.style = "animation: jump 450ms linear";
     
     setTimeout(() => {
-        document.querySelector("#dino").style = "animation: none";
+        dino.style = "animation: none";
     }, 450);
 }
 
 let checkScore = setInterval(function() {
     let treeLeft =
-    parseInt(window.getComputedStyle(document.querySelector(".bi-tree-fill")).getPropertyValue("left"));
+    parseInt(window.getComputedStyle(tree).getPropertyValue("left"));
     if (treeLeft < 100) {
         count ++;
     }
@@ -21,19 +21,19 @@ let checkScore = setInterval(function() {
 }, 250);
 
 if (score > 100) {
-    document.querySelector(".bi-tree-fill").style = "animation: tree 1.8s infinite linear;";
+    tree.style = "animation: tree 1.6s infinite linear;";
 }
 
 if (score > 200) {
-    document.querySelector(".bi-tree-fill").style = "animation: tree 1.6s infinite linear;";
+    tree.style = "animation: tree 1.4s infinite linear;";
 }
 
 if (score > 300) {
-    document.querySelector(".bi-tree-fill").style = "animation: tree 1.4s infinite linear;";
+    tree.style = "animation: tree 1.2s infinite linear;";
 }
 
 function Reload() {
-    document.querySelector(".bi-tree-fill").style = "animation: tree 2s infinite linear;";
+    tree.style = "animation: tree 1.8s infinite linear;";
     document.querySelector("#block").style = "animation-delay: 1s;"
     document.querySelector("#game-over").style = "display: none;"
     score.innerHTML = 0;
@@ -44,14 +44,14 @@ let checkDead = setInterval(function() {
     let dinoTop = 
     parseInt(window.getComputedStyle(document.getElementById("dino")).getPropertyValue("top"));
     let treeLeft =
-    parseInt(window.getComputedStyle(document.querySelector(".bi-tree-fill")).getPropertyValue("left"));
+    parseInt(window.getComputedStyle(tree).getPropertyValue("left"));
     
     if (treeLeft > 100 && treeLeft < 165 && dinoTop > 295) {
-        document.querySelector(".bi-tree-fill").style = "animation: none"
+        tree.style = "animation: none"
         document.querySelector("#game-over").style = "display: block;"
     }
     let dinoRight = dino.getBoundingClientRect(dino).left;
     // parseInt(window.getComputedStyle(document.getElementById("dino")).getPropertyValue("right"));
 
-    console.log(dinoRight)
+    // console.log(dinoRight)
 }, 10);
