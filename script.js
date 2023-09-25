f84ac3251cf65e7f90815c839c6d2ec66ebbc9f8
izima01 <kingsleyizima@gmail.com>
2023-09-25 20:47:52 +0100

    added modifications

--- script.js
+++ script.js
@@ -1,36 +1,32 @@
-let dino = document.querySelector("#dino")
-let score = document.querySelector("#scoreValue")
-let tree = document.querySelector(".bi-tree-fill")
+let dino = document.querySelector("#dino");
+let score = document.querySelector("#scoreValue");
+let game = document.querySelector("#game");
+let tree = document.querySelector("#block");
+let startBtn = document.querySelector("#start");
 let count = 0;
+let isRunning = false;
+let isFailed = false;
 
 function jump() {
-    dino.style = "animation: jump 450ms linear";
-    
+    dino.style = "animation: jump 960ms linear";
     setTimeout(() => {
         dino.style = "animation: none";
     }, 450);
 }
 
 let checkScore = setInterval(function() {
-    let treeLeft =
-    parseInt(window.getComputedStyle(tree).getPropertyValue("left"));
+    let treeLeft = parseInt(window.getComputedStyle(tree).getPropertyValue("left"));
     if (treeLeft < 100) {
         count ++;
     }
     score.innerHTML = count * 10;
 }, 250);
 
-if (score > 100) {
-    tree.style = "animation: tree 1.6s infinite linear;";
-}
+if (score > 100) tree.style = "animation: tree 1.6s infinite linear;";
 
-if (score > 200) {
-    tree.style = "animation: tree 1.4s infinite linear;";
-}
+if (score > 200) tree.style = "animation: tree 1.4s infinite linear;";
 
-if (score > 300) {
-    tree.style = "animation: tree 1.2s infinite linear;";
-}
+if (score > 300) tree.style = "animation: tree 1.2s infinite linear;";
 
 function Reload() {
     tree.style = "animation: tree 1.8s infinite linear;";
@@ -38,20 +34,40 @@ function Reload() {
     document.querySelector("#game-over").style = "display: none;"
     score.innerHTML = 0;
     count = 0;
+    isRunning =true;
+    isFailed =false;
 }
 
 let checkDead = setInterval(function() {
-    let dinoTop = 
-    parseInt(window.getComputedStyle(document.getElementById("dino")).getPropertyValue("top"));
-    let treeLeft =
-    parseInt(window.getComputedStyle(tree).getPropertyValue("left"));
-    
-    if (treeLeft > 100 && treeLeft < 165 && dinoTop > 295) {
-        tree.style = "animation: none"
-        document.querySelector("#game-over").style = "display: block;"
-    }
-    let dinoRight = dino.getBoundingClientRect(dino).left;
-    // parseInt(window.getComputedStyle(document.getElementById("dino")).getPropertyValue("right"));
+    let dinoBottom = parseInt(window.getComputedStyle(document.getElementById("dino")).getPropertyValue("bottom"));
+    let treeLeft = parseInt(window.getComputedStyle(tree).getPropertyValue("left"));
 
-    // console.log(dinoRight)
+    if (treeLeft > 102 && treeLeft < 160 && dinoBottom < 32) {
+        isFailed = true;
+        tree.style = "animation: none";
+        tree.style.left = `${treeLeft}px`;
+        dino.style.bottom = `${dinoBottom}px`;
+        isRunning = false;
+    }
+    if (isFailed) startBtn.innerHTML = "Reload";
+    if (isRunning) startBtn.style.display = "none";
+    if (!isRunning) startBtn.style.display = "block";
+    if (isFailed) startBtn.className = "reload";
+    if (isRunning && !isFailed) startBtn.id = "start";
 }, 10);
+
+
+startBtn.addEventListener("click", () => {
+    tree.classList.add("startMoving");
+    isRunning = true;
+    if (isFailed) Reload();
+});
+
+game.addEventListener("click", (e) => {
+    if (e.target.id == "start") return;
+    if (isRunning) return jump();
+});
+
+document.onkeydown = () => {
+    if (isRunning) return jump();
+}
